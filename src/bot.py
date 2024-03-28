0684991a7fbc5ffb764444d7006dc75fcccbe0d5
Victor Bello <victorbello@macs-MacBook-Pro-2.local>
2024-03-28 09:33:21 +0100

    cder

--- src/bot.py
+++ src/bot.py
@@ -24,8 +24,7 @@ import db
 import layouts
 from callback_actions import CustomCallBacksActions
 import bot_helpers
-import argparse
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket
 
 class UserType(Enum):
     NO_USER = 1
@@ -403,36 +402,6 @@ async def main() -> None:
     await dp.start_polling(bot)
 
 
-class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-
-
-def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8089):
-    server_address = (addr, port)
-    httpd = server_class(server_address, handler_class)
-
-    print(f"Starting httpd server on {addr}:{port}")
-    httpd.serve_forever()
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
-    parser.add_argument(
-        "-l",
-        "--listen",
-        default="localhost",
-        help="Specify the IP address on which the server listens",
-    )
-    parser.add_argument(
-        "-p",
-        "--port",
-        type=int,
-        default=8089,
-        help="Specify the port on which the server listens",
-    )
-    args = parser.parse_args()
-    run(addr=args.listen, port=args.port)
